224c97776a8e6a122aca2ddbfe6ce4516c656003
blackeye75 <priyanshuraj22275@gmail.com>
2024-09-18 19:18:20 +0530

    #

---
+++ src/app/chai/layout.tsx
@@ -0,0 +1,16 @@
+export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <h1>Nav Items</h1>
+          {children}
+        </body>
+      </html>
+    );
+  }
---
+++ src/app/chai/page.tsx
@@ -0,0 +1,5 @@
+export default function chai() {
+    return (
+      <h1 className="text-2xl text-center">Hello page</h1>
+    );
+  }
