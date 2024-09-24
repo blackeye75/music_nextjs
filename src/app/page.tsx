c80e301216583c6b23abb1b40d3518e1fae1d360
blackeye75 <priyanshuraj22275@gmail.com>
2024-09-24 19:04:48 +0530

    card design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { HeroSection } from "@/components/HeroSection";
 import Image from "next/image";
+import {FeaturedCourses }from "@/components/FeaturedCourses"
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]" >
       {/* <h1 className="text-2xl text-center" >chai or code</h1> */}
       <HeroSection />
-
+      <FeaturedCourses />
     </main>
   );
 }
