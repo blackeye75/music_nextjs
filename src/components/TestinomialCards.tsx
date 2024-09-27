5c4de21b9deb6ee53278e260165e2f0f3021b116
blackeye75 <priyanshuraj22275@gmail.com>
2024-09-27 17:23:34 +0530

    background grid added

--- src/components/TestinomialCards.tsx
+++ src/components/TestinomialCards.tsx
@@ -36,10 +36,20 @@ const musicSchoolTestimonials = [
 
 function MusicSchoolTestimonials() {
   return (
-    <div className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
-        <h2 className="text-3xl font-bold text-center mb-8 z-10">Hear our Harmony: Voices of success</h2>
-        <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
+    <div className="h-fit w-full dark:bg-black dark:bg-grid-white/[0.1] relative flex flex-col items-center justify-center overflow-hidden">
+        <h2 className="text-4xl text-blue-500 mt-3 font-bold text-center mb-8 z-10">Hear our Harmony: Voices of success</h2>
+        <div className="flex justify-center w-full overflow-hidden px-4 sm:px-1 lg:px-8">
             <div className="w-full max-w-6xl">
+            <InfiniteMovingCards
+                items={musicSchoolTestimonials}
+                direction="right"
+                speed="slow"
+      />
+            <InfiniteMovingCards
+                items={musicSchoolTestimonials}
+                direction="left"
+                speed="slow"
+      />
             <InfiniteMovingCards
                 items={musicSchoolTestimonials}
                 direction="right"
