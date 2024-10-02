17f954705f43545eb3a622a3c0fbc0f67dc3acca
blackeye75 <priyanshuraj22275@gmail.com>
2024-10-02 18:37:59 +0530

    ui fixes

--- src/components/UpcomingWebinars.tsx
+++ src/components/UpcomingWebinars.tsx
@@ -52,12 +52,12 @@ const featuredWebinars = [
 const UpcomingWebinars = () => {
     return (
         <div className='p-12 bg-gray-900'  >
-            <div className='max-w-7xl mx-auto px-4 sm:px-6' >
+            <div className='sm:max-w-7xl max-w-8xl mx-auto px-4 sm:px-6' >
                 <div className='text-center' >
                     <h2 className='text-base text-teal-600 font-semibold tracking-wide uppercase' >Featured Webinars</h2>
                     <p className='mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl' >ENHANCE YOUR MUSICAL JOURNEY!</p>
                 </div>
-                <div className='m-10 ' >
+                <div className=' w-full' >
                     <HoverEffect
                    
                         items={featuredWebinars.map(webinar => (
